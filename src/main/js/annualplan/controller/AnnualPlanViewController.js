0af9c5aee8eda7fb895303c08d62fa9ef236026d
Marcin Jarzembowski <marcin.jarzembowski@allegrogroup.com>
2015-04-28 16:29:16 +0200

    Skeleton of annual view
    Change-Id: Iedb0da3cc033b30ef3a523f1fbf7173fe65c1dcc

--- src/main/js/annualplan/controller/AnnualPlanViewController.js
+++ src/main/js/annualplan/controller/AnnualPlanViewController.js
@@ -1,2 +1,9 @@
 annualPlanModule.controller('AnnualPlanViewController', ['$scope', function ($scope) {
+<<<<<<< HEAD
+=======
+    this.actualYear = 2015;
+    this.annualPlanColumns = {1,31};
+    this.annualPlan = null;
+
+>>>>>>> Skeleton of annual view
  }]);
