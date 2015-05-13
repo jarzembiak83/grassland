87bb3c36b06b13348615e0a95af8eaf63c145ac0
Marcin Jarzembowski <marcin.jarzembowski@allegrogroup.com>
2015-05-13 17:03:21 +0200

    rendering weekend days - not finished
    Change-Id: Ia7093896bc857a19b0b164cd273217f8d7b6924b

--- src/main/js/annualplan/directive/SingleDayPanel.js
+++ src/main/js/annualplan/directive/SingleDayPanel.js
@@ -1,6 +1,18 @@
 annualPlanModule.directive('singleDayPanel', function(){
     return {
         restrict: 'E',
-        templateUrl: '/templates/annualplan/single-day-panel.tpl.html'
+        scope: {
+            day: '=?'
+        },
+        templateUrl: '/templates/annualplan/single-day-panel.tpl.html',
+        link : function (scope) {
+            scope.isWeekendDay = function() {
+                return (scope.day.date.getDay() % 6 == 0);
+            };
+            
+            scope.isEmptyDay = function() {
+                return (scope.day);
+            };
+        }
     }
 });
